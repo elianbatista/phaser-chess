64f7c2fe2dff285e051654748a58376daf5d105b
Elian Batista <elianbatista2000@gmail.com>
2021-03-02 23:10:08 -0300

    game js

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,6 @@
 const config = {
-  width: 800,
-  height: 600,
+  width: 1280,
+  height: 720,
   type: Phaser.AUTO,
   audio: {
     disableWebAudio: true
@@ -12,6 +12,96 @@ const config = {
       gravity: {y : 0},
     }
   },
+  scene: {
+    preload: preload,
+    create: create
+  }
 };
 
 const game = new Phaser.Game(config);
+
+var pieces = [
+  'bispo-branco',
+  'bispo-preto',
+  'cavalo-branco',
+  'cavalo-preto',
+  'peao-branco',
+  'peao-preto',
+  'rainha-branco',
+  'rainha-preto',
+  'rei-branco',
+  'rei-preto',
+  'torre-branco',
+  'torre-preto'
+]
+
+/*
+  bispo branco 0
+  bispo preto 1
+  cavalo branco 2
+  cavalo preto 3
+  peao branco 4
+  peao preto 5
+  rainha branco 6
+  rainha preto 7
+  rei branco 8
+  rei preto 9
+  torre branco 10
+  torre preto 11
+  casa vazia 12
+*/
+
+var initialPositions = [
+  [11, 3, 1, 7, 9, 1, 3, 11],
+  [5, 5, 5, 5, 5, 5, 5, 5],
+  [12, 12, 12, 12, 12, 12, 12, 12],
+  [12, 12, 12, 12, 12, 12, 12, 12],
+  [12, 12, 12, 12, 12, 12, 12, 12],
+  [12, 12, 12, 12, 12, 12, 12, 12],
+  [4, 4, 4, 4, 4, 4, 4, 4],
+  [10, 2, 0, 6, 8, 0, 2, 10]
+]
+
+function preload () {
+  pieces.forEach((piece, index) => {
+    this.load.image(piece, `sprites/pieces/${piece}.png`)
+  })
+  this.load.audio('piece-move', '')
+}
+
+function create () {
+  var squareSize = 80
+  var whitehouses = this.add.graphics({ fillStyle: { color: 0xFFFFFF } })
+  var blackhouses = this.add.graphics({ fillStyle: { color: 22222222 } })
+  for(let i = 0; i < 8; i++) {
+    for(let j = 0; j < 8; j++) {
+      var rect = new Phaser.Geom.Rectangle(squareSize * j, squareSize * i, squareSize, squareSize)
+      if((i + j) % 2 != 0) {
+        blackhouses.fillRectShape(rect)
+      } else {
+        whitehouses.fillRectShape(rect)
+      }
+      console.log(initialPositions[i][j])
+      if(initialPositions[i][j] != 12) {
+        var piece = this.add.sprite((squareSize / 2) + (squareSize * j), (squareSize / 2) + (squareSize * i), pieces[initialPositions[i][j]]).setInteractive({ cursor: 'pointer' })
+        this.input.setDraggable(piece)
+        piece.displayWidth = squareSize
+        piece.scaleY = piece.scaleX
+      }
+    }
+  }
+  this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+    gameObject.x = dragX
+    gameObject.y = dragY
+  })
+  this.input.on('dragend', (pointer, gameObject) => {
+    console.log(Math.round((gameObject.x + 80 / 2) / 80))
+    console.log(Math.round((gameObject.y + 80 / 2) / 80))
+    gameObject.x = (Math.floor(gameObject.x / 80) * 80) + 80 / 2
+    gameObject.y = (Math.floor(gameObject.y / 80) * 80) + 80 / 2
+  })
+}
+
+function handle() {
+  console.log('shape')
+}
