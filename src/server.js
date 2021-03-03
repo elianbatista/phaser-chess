aa207a3b91953f558cf920f6a092d7600b842b8f
Elian Batista <elianbatista2000@gmail.com>
2021-03-02 23:10:57 -0300

    server js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/../public'))
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/../index.html')
-})
+app.use('/', express.static(__dirname + '/../public'))
 
 app.listen(8080, () => {
   console.log('Server listening on http://localhost:8080')
